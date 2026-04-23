e317e2dd93a78a5ca02c5b7c65e32c96c9b64098
LothaireE <lothaire.epee@gmail.com>
2026-04-23 11:01:37 +0200

    Route handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeProvider.tsx";
 import { LanguageProvider } from "./context/LanguageContext";
 import { ErrorBoundary } from "./components/ErrorBoundary.tsx";
 
-// const router = createBrowserRouter([
-//   { path: "/*", element: <App /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "*", element: <Navigate to="/" replace /> },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <LanguageProvider>
-          <App />
+          <RouterProvider router={router} />
         </LanguageProvider>
       </ThemeProvider>
     </ErrorBoundary>
